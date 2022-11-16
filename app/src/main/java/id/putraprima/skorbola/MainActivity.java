161c7cfae9fd0ff8d67bce4d091e0609da9cee9f
Muflih <69343587+mufmufmuf@users.noreply.github.com>
2022-11-16 19:54:13 +0700

    Tugas 5 Praktikum Mobile Shift H Muhammad Muflih Abdurrafi H1D020033

--- app/src/main/java/id/putraprima/skorbola/MainActivity.java
+++ app/src/main/java/id/putraprima/skorbola/MainActivity.java
@@ -1,15 +1,43 @@
 package id.putraprima.skorbola;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button next;
+    ImageView homeLogo, awayLogo;
+    Uri homeImg, awayImg;
+    EditText homeInput, awayInput;
+    String s_home, s_away, s_homeimg, s_awayimg;
+    private static final String TAG = MainActivity.class.getCanonicalName();
+    private static final int HOME_REQUEST_CODE = 1;
+    private static final int AWAY_REQUEST_CODE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        next = findViewById(R.id.btn_team);
+        homeLogo = findViewById(R.id.home_logo);
+        awayLogo = findViewById(R.id.away_logo);
+        homeInput = findViewById(R.id.home_team);
+        awayInput = findViewById(R.id.away_team);
         //TODO
         //Fitur Main Activity
         //1. Validasi Input Home Team
@@ -17,5 +45,68 @@ public class MainActivity extends AppCompatActivity {
         //3. Ganti Logo Home Team
         //4. Ganti Logo Away Team
         //5. Next Button Pindah Ke MatchActivity
+
+        homeLogo.setOnClickListener(new View.OnClickListener(){
+           public void onClick(View view){
+               startActivityForResult(new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI),HOME_REQUEST_CODE);
+           }
+        });
+        awayLogo.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                startActivityForResult(new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI),AWAY_REQUEST_CODE);
+            }
+        });
+        next.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                s_home = homeInput.getText().toString();
+                s_away = awayInput.getText().toString();
+
+                if (homeImg!=null && awayImg!=null){
+                    s_homeimg = homeImg.toString();
+                    s_awayimg = awayImg.toString();
+                    Intent intent = new Intent(MainActivity.this, MatchActivity.class);
+                    intent.putExtra("key_home",s_home);
+                    intent.putExtra("key_away",s_away);
+                    intent.putExtra("key_imagehome", s_homeimg);
+                    intent.putExtra("key_imageaway", s_awayimg);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity.this, "Tolong masukkan gambar", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_CANCELED){
+            Log.d(TAG, "pilih gambar dicancel");
+            return;
+        } else if (requestCode == HOME_REQUEST_CODE){
+            if(data!=null){
+                try {
+                    Uri imageUri = data.getData();
+                    homeImg = imageUri;
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), homeImg);
+                    homeLogo.setImageBitmap(bitmap);
+                } catch (IOException error){
+                    Toast.makeText(this, "Can't Load Image",Toast.LENGTH_SHORT).show();
+                    Log.e(TAG, error.getMessage());
+                }
+            }
+        } else if (requestCode == AWAY_REQUEST_CODE){
+            if(data!=null){
+                try {
+                    Uri imageUri = data.getData();
+                    awayImg = imageUri;
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), awayImg);
+                    awayLogo.setImageBitmap(bitmap);
+                } catch (IOException error){
+                    Toast.makeText(this, "Can't Load Image",Toast.LENGTH_SHORT).show();
+                    Log.e(TAG, error.getMessage());
+                }
+            }
+        }
     }
 }
--- app/src/main/java/id/putraprima/skorbola/MatchActivity.java
+++ app/src/main/java/id/putraprima/skorbola/MatchActivity.java
@@ -2,9 +2,20 @@ package id.putraprima.skorbola;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MatchActivity extends AppCompatActivity {
+
+    TextView homeTxt, awayTxt, homeScore, awayScore;
+    ImageView homeLogo, awayLogo;
+    Button result, reset, add_home_satu, add_home_dua, add_home_tiga, add_away_satu, add_away_dua, add_away_tiga;
+    int skor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +25,107 @@ public class MatchActivity extends AppCompatActivity {
         //1.Menampilkan detail match sesuai data dari main activity
         //2.Tombol add score menambahkan satu angka dari angka 0, setiap kali di tekan
         //3.Tombol Cek Result menghitung pemenang dari kedua tim dan mengirim nama pemenang ke ResultActivity, jika seri di kirim text "Draw"
+
+        String home = getIntent().getExtras().getString("key_home");
+        final String away = getIntent().getExtras().getString("key_away");
+        Uri bitmapHome = Uri.parse(getIntent().getExtras().getString("key_imagehome"));
+        Uri bitmapAway = Uri.parse(getIntent().getExtras().getString("key_imageaway"));
+
+        homeTxt = findViewById(R.id.txt_home);
+        awayTxt = findViewById(R.id.txt_away);
+        homeLogo = findViewById(R.id.home_logo);
+        awayLogo = findViewById(R.id.away_logo);
+        reset = findViewById(R.id.btn_reset);
+        result = findViewById(R.id.btn_result);
+        homeScore = findViewById(R.id.score_home);
+        awayScore = findViewById(R.id.score_away);
+        add_home_satu = findViewById(R.id.btn_add_home_satu);
+        add_home_dua = findViewById(R.id.btn_add_home_dua);
+        add_home_tiga = findViewById(R.id.btn_add_home_tiga);
+        add_away_satu = findViewById(R.id.btn_add_away_satu);
+        add_away_dua = findViewById(R.id.btn_add_away_dua);
+        add_away_tiga = findViewById(R.id.btn_add_away_tiga);
+
+        homeTxt.setText(home);
+        awayTxt.setText(away);
+        homeLogo.setImageURI(bitmapHome);
+        awayLogo.setImageURI(bitmapAway);
+
+
+        add_home_satu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String score = homeScore.getText().toString();
+                int i = Integer.parseInt(score);
+                i+=1;
+                String a = new Integer(i).toString();
+                homeScore.setText(a);
+            }
+        });
+        add_home_dua.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String score = homeScore.getText().toString();
+                int i = Integer.parseInt(score);
+                i+=2;
+                String a = new Integer(i).toString();
+                homeScore.setText(a);
+            }
+        });
+        add_home_tiga.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String score = homeScore.getText().toString();
+                int i = Integer.parseInt(score);
+                i+=3;
+                String a = new Integer(i).toString();
+                homeScore.setText(a);
+            }
+        });
+        add_away_satu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String score = awayScore.getText().toString();
+                int i = Integer.parseInt(score);
+                i+=1;
+                String a = new Integer(i).toString();
+                awayScore.setText(a);
+            }
+        });
+        add_away_dua.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String score = awayScore.getText().toString();
+                int i = Integer.parseInt(score);
+                i+=2;
+                String a = new Integer(i).toString();
+                awayScore.setText(a);
+            }
+        });
+        add_away_tiga.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String score = awayScore.getText().toString();
+                int i = Integer.parseInt(score);
+                i+=3;
+                String a = new Integer(i).toString();
+                awayScore.setText(a);
+            }
+        });
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                homeScore.setText("0");
+                awayScore.setText("0");
+            }
+        });
+        result.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MatchActivity.this, ResultActivity.class);
+
+                startActivity(intent);
+            }
+        });
     }
 }
